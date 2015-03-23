69427d6354c58fa30fb5b6e03108780753424681
Oliver Tessmer <oliver.tessmer@gmail.com>
2015-03-23 00:37:11 -0400

    moved navigation to helper

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -2,4 +2,12 @@ module StaticHelper
   def current_date_and_time
     Time.now.strftime('%B %-d, %Y at %-l:%M%P')
   end
+  
+  def navigation
+<ul>
+  <li><%= link_to 'Home', root_path %></li>
+  <li><%= link_to 'About', about_path %></li>
+  <li><%= link_to 'Cat Pictures', cat_pictures_path %></li>
+</ul>
+  end
 end
