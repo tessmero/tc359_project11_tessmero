bee29ad5942b6492dda5799d875c665f269de5d3
Oliver Tessmer <oliver.tessmer@gmail.com>
2015-03-23 00:43:59 -0400

    moved navigation to helper

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,9 +1,2 @@
 module StaticHelper  
-  def navigation
-	'<ul>
-	  <li><%= link_to 'Home', root_path %></li>
-	  <li><%= link_to 'About', about_path %></li>
-	  <li><%= link_to 'Cat Pictures', cat_pictures_path %></li>
-	</ul>'
-  end
 end
