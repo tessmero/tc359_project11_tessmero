b65dd5077fd68f0099c4f2e58e59eaf3b943ae2e
Oliver Tessmer <oliver.tessmer@gmail.com>
2015-03-24 19:49:55 -0400

    switched to soft tabs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
-	def cats
-	  @cats = CatAPI.new.get_images(results_per_page: params[:number_of_cats] || 1)
-	end
+  def cats
+    @cats = CatAPI.new.get_images(results_per_page: params[:number_of_cats] || 1)
+  end
 
   def home
   end
